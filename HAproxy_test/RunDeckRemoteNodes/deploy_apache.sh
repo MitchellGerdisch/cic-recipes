663487707462ebf593b8e72313572f50de9e3362
MitchellGerdisch <mitchell.gerdisch@alcatel-lucent.com>
2012-12-13 10:20:11 -0600

    testing haproxy stuff

---
+++ HAproxy_test/RunDeckRemoteNodes/deploy_apache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# args:
+# $1 the error code of the last command (should be explicitly passed)
+# $2 the message to print in case of an error
+# 
+# an error message is printed and the script exists with the provided error code
+function error_exit {
+	echo "$2 : error code: $1"
+	exit ${1}
+}
+
+
+echo "Using yum. Installing httpd on one of the following : Red Hat, CentOS, Fedora, Amazon"
+yum install -y -q httpd || error_exit $? "Failed on: sudo yum install -y -q httpd"
+	
+service httpd start
+
+echo "You have reached a remote node apache deployment" > /var/www/html/index.html
+ 
+
+
